b81ed627865a6443c4e35a22cf713213a914734d
ShlokD <shlokpdesai@gmail.com>
2019-04-12 15:49:20 -0700

    Add board and score

--- js/app.js
+++ js/app.js
@@ -22,19 +22,103 @@ class Piece {
   }
 }
 
+class Scores {
+  constructor(scoreEl, availableEl) {
+    this.scoreEl = scoreEl;
+    this.availableEl = availableEl;
+  }
+
+  setScore(score) {
+    this.scoreEl.textContent = `${score}`;
+  }
+
+  setAvailable(available) {
+    this.availableEl.textContent = `${available}`
+  }
+}
+
 class Board {
   constructor() {
     this.piece = new Piece(document.querySelector("#piece"));
+    this.scores = new Scores(
+      document.querySelector("#score"),
+      document.querySelector("#available")
+    );
+
     this.handleKeyPress = this.handleKeyPress.bind(this);
     this.start = this.start.bind(this);
+
+    this.layout = new Array(7).fill([...new Array(7)])
+    this.positionMap = {
+      37: [-1, 0], //left
+      38: [0, 1], //up
+      39: [1, 0], // right
+      40: [0, -1] // down
+    }
+    this.positionX = 3;
+    this.positionY = 3;
+    this.score = 0;
+    this.available = 0;
+    this.seen = {};
+  }
+
+ 
+
+  generate() {
+    this.layout.forEach(row => {
+     for(let i = 0; i < row.length; ++i) {
+       row[i] = Math.round(Math.random())
+       if(row[i] === 1) this.available++;
+     }
+    })
+
+    this.layout[3][3] = 1;
+    this.seen[`33`] = true;
+
+    this.scores.setScore(this.score);
+    this.scores.setAvailable(this.available);
+  }
+
+  setPosition(direction) {
+    if(!this.positionMap[direction]) return false;
+
+    const [x, y] = this.positionMap[direction];
+    if(this.positionX + x < 0 || this.positionX + x > 6) return false;
+    if(this.positionY + y < 0 || this.positionY + y > 6) return false;
+
+    const newX = this.positionX + x;
+    const newY = this.positionY + y;
+    const posAvailable = !!this.layout[newX][newY];
+
+    if(posAvailable) {
+      this.positionX = newX;
+      this.positionY = newY
+    }
+    return posAvailable;
+  }
+
+  calculateScore() {
+    if(!this.seen[`${this.positionX}${this.positionY}`]) {
+      this.seen[`${this.positionX}${this.positionY}`] = true;
+      this.score++;
+      this.scores.setScore(this.score);
+    }
   }
 
   handleKeyPress(ev) {
     ev.preventDefault();
-    this.piece.move(ev.keyCode);
+    const { keyCode } = ev;
+
+    const isPositionSet = this.setPosition(keyCode);
+    console.log({ x: this.positionX, y: this.positionY, score: this.score, available: this.available})
+    if(isPositionSet) {
+      this.calculateScore();
+      this.piece.move(keyCode);
+    }
   }
 
   start() {
+    this.generate();
     document.addEventListener('keydown', this.handleKeyPress);
   }
 }
