d8a3cd0e1f964d0c3f4b04c17b44b12fd0cf48b9
ShlokD <shlokpdesai@gmail.com>
2019-04-14 13:50:28 -0700

    Add builds

---
+++ sw.js
@@ -0,0 +1,33 @@
+importScripts('https://storage.googleapis.com/workbox-cdn/releases/4.2.0/workbox-sw.js');
+
+
+if(workbox) {
+  workbox.routing.registerRoute(
+    new RegExp('\\.js$'),
+    new workbox.strategies.StaleWhileRevalidate({
+      cacheName: 'js-cache'
+    })
+  );
+
+  workbox.routing.registerRoute(
+    new RegExp('\\.css$'),
+    new workbox.strategies.StaleWhileRevalidate({
+      cacheName: 'css-cache',
+    })
+  );
+
+  workbox.routing.registerRoute(
+    '/',
+    new workbox.strategies.StaleWhileRevalidate({
+      cacheName: 'html-cache',
+    })
+  );
+
+  workbox.routing.registerRoute(
+    new RegExp('\\.(ico|png)$'),
+    new workbox.strategies.CacheFirst({
+      cacheName: 'assets-cache',
+    })
+  );
+  
+}
